733ca63002d6850b57aca848c020bcc4129db016
David Holmstedt <david.holmstedt@cambio.se>
2024-10-30 13:19:40 +0100

    Added build files for admin and user

--- build-admin.sh
+++ build-admin.sh
@@ -7,7 +7,7 @@ then
 fi
 
 # Add the icon as a resource to the executable and add the manifest
-rsrc -ico assets/dock-window.ico -manifest app.manifest -o rsrc.syso
+rsrc -ico assets/dock-window.ico -manifest app-admin.manifest -o rsrc.syso
 
 # Build for Windows without console
 go build -ldflags="-H windowsgui" -o telewindow.exe
---
+++ build-user.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install rsrc tool
+if ! command -v rsrc &> /dev/null
+then
+    go install -v github.com/akavel/rsrc@latest
+fi
+
+# Add the icon as a resource to the executable and add the manifest
+rsrc -ico assets/dock-window.ico -o rsrc.syso
+
+# Build for Windows without console
+go build -ldflags="-H windowsgui" -o telewindow.exe
